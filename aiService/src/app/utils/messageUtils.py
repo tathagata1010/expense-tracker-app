7d7792a69bc472ec12d1332b65841148a000a45d
tathagata1010 <53346012+tathagata1010@users.noreply.github.com>
2024-08-21 10:30:44 +0530

    added exception handling

--- aiService/src/app/utils/messageUtils.py
+++ aiService/src/app/utils/messageUtils.py
@@ -3,7 +3,7 @@ import re
 class MessageUtils:
     def isBankSms(self, message):
         # Define the list of words to check for
-        words_to_check = ["withdrawal", "sent", "debit", "credit", "card", "credited", "balance", "statement", "A/C"]
+        words_to_check = ["withdrawal", "sent", "debit", "credit", "card", "credited","Avl Lmt", "balance", "statement", "A/C"]
         
         # Create a regular expression pattern that matches any of the words
         pattern = r'\b(?:' + '|'.join(words_to_check) + r')\b'
